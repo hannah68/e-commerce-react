0f532f3e8fa9e30178613268538e990b51bfd53b
hannah68 <reihaneh.naderi1989@gmail.com>
2022-04-17 17:42:06 +0100

    second attempt

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+const path = require('path');
+const express = require('express');
 const router = jsonServer.router('./db/db.json');
-const middlewares = jsonServer.defaults({static: './build'});
-
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3030;
 
 server.use(middlewares);
 server.use(router);
+server.use(express.static(path.join(__dirname, 'build')));
+
+server.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 server.listen(port);
