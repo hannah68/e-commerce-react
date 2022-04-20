ebc5e6c4ca186bab493a67c648f918b4903232e0
hannah68 <reihaneh.naderi1989@gmail.com>
2022-04-20 13:46:48 +0100

    update server

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
+const path = require("path");
+const express = require("express");
+const app = express();
+
 const router = jsonServer.router("./db/db.json");
-const middlewares = jsonServer.defaults();
+// const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3030;
-
-server.use(middlewares);
+app.use("/", express.static("build"), router);
+// server.use(middlewares);
 // server.use(
 // 	jsonServer.rewriter({
 // 		"/api/*": "/$1",
 // 	})
 // );
-server.use(router);
+if (process.env.NODE_ENV === "production") {
+	app.get("*", function (req, res) {
+	  res.sendFile(path.resolve(__dirname + "./build/index.html"));
+	});
+  }
+  
 server.listen(port, () => {
 	console.log(`Server is running on ${port}`);
 });
